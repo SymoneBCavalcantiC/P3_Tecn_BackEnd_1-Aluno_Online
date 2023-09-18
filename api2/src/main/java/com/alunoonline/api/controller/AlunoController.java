91ce57e57c74ab53c0fabe463ffaff34001534ef
SymoneBCavalcantiC <2022210220016@iesp.edu.br>
2023-09-18 06:39:03 -0300

    Conteudo da aula 120923 parte 2

--- api2/src/main/java/com/alunoonline/api/controller/AlunoController.java
+++ api2/src/main/java/com/alunoonline/api/controller/AlunoController.java
@@ -5,10 +5,8 @@ import com.alunoonline.api.repository.AlunoRepository;
 import com.alunoonline.api.service.AlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/aluno")
@@ -19,4 +17,9 @@ public class AlunoController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Aluno> create(@RequestBody Aluno aluno){
+        Aluno alunoCreated = service.create(aluno);
+
+        return ResponseEntity.status(201).body(alunoCreated);
+    }
 }
