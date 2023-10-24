e991b18a19182fae9bc5e6d18bb670c2e45b55bf
SymoneBCavalcantiC <2022210220016@iesp.edu.br>
2023-10-24 09:36:24 -0300

    Conteudo da aula101023 parte 2: ProfessorController e testando funcionamento (postman + pgAdmin)

--- api2/src/main/java/com/alunoonline/api/controller/ProfessorController.java
+++ api2/src/main/java/com/alunoonline/api/controller/ProfessorController.java
@@ -3,16 +3,19 @@ package com.alunoonline.api.controller;
 import com.alunoonline.api.model.Professor;
 import com.alunoonline.api.service.ProfessorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/professor")
 public class ProfessorController {
 
     @Autowired
     ProfessorService service;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Professor> create(@RequestBody Professor professor){
         Professor professorCreted = service.create(professor);
 
